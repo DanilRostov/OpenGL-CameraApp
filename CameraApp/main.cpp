598d99540c0658dd0cf885aed1a3517d317f478f
Danil Rostov <daniil.rossy@gmail.com>
2025-04-21 17:48:08 -0700

    Initial Commit

---
+++ CameraApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CameraApp
+//
+//  Created by Danil Rostov on 4/21/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
